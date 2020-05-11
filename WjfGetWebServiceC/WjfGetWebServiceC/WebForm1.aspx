646c1cfea0eea503f7c78bb06fa0ac7f6894ae7e
wjfjfw2854 <421284553@qq.com>
2020-05-11 10:29:51 +0800

    前端及后端demo

---
+++ WjfGetWebServiceC/WjfGetWebServiceC/WebForm1.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WjfGetWebServiceC.WebForm1" %>
+<%--<%@ Import Namespace="WjfGetWebServiceC.WjfWebService" %>--%>
+<%@ Import Namespace="WjfGetWebServiceC.WjfWebService" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Value1:<asp:TextBox ID="txtValue1" runat="server"></asp:TextBox>
+            Value2:<asp:TextBox ID="txtValue2" runat="server"></asp:TextBox><br />
+            Multiple:<asp:TextBox ID="txtMultiple" runat="server"></asp:TextBox><br />
+            <asp:Button ID="btnOk" runat="server" Text="WebService" OnClick="btnOk_Click"></asp:Button>
+        </div>
+    </form>
+</body>
+</html>
+<script runat="server">
+    protected void btnOk_Click(object sender, System.EventArgs e){
+        WjfGetWebServiceC.WjfWebService.WebService1 wjfWebService = new WjfGetWebServiceC.WjfWebService.WebService1();
+        int i = Convert.ToInt32(txtValue1.Text);
+        int j = Convert.ToInt32(txtValue2.Text);
+        txtMultiple.Text = wjfWebService.Multiple(i, j).ToString();
+    }
+</script>
---
+++ WjfGetWebServiceC/WjfGetWebServiceC/WebForm1.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <自动生成>
+//     此代码由工具生成。
+//
+//     对此文件的更改可能导致不正确的行为，如果
+//     重新生成代码，则所做更改将丢失。
+// </自动生成>
+//------------------------------------------------------------------------------
+
+namespace WjfGetWebServiceC
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// form1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// txtValue1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtValue1;
+
+        /// <summary>
+        /// txtValue2 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtValue2;
+
+        /// <summary>
+        /// txtMultiple 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtMultiple;
+
+        /// <summary>
+        /// btnOk 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnOk;
+    }
+}
